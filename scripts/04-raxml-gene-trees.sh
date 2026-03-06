2353e66d34c96de4db346a54d4323e2c21b3b70a
nathanlin-uw <ntlin@wisc.edu>
2026-03-06 14:18:20 -0600

    Added the redundant-runs-avoiding checks to 06-raxml-10mb-concatenation, edited 04-raxml-gene-trees.sh to store everything in a new /RAxML/gene_trees/ directory, and added R scripts for making the supermatrix and the 10Mb windows.

--- scripts/04-raxml-gene-trees.sh
+++ scripts/04-raxml-gene-trees.sh
@@ -6,9 +6,10 @@ run_description="all_genes"
 
 mkdir ../results/
 mkdir ../results/RAxML/
+mkdir ../results/RAxML/gene_trees
 
 # We'll store the ones we already did here
-completed_filenames="../results/RAxML/${run_description}_log_completed_files.txt"
+completed_filenames="../results/RAxML/gene_trees/${run_description}_log_completed_files.txt"
 touch "$completed_filenames"
 
 for file in "$DATADIR"/*; do
@@ -20,7 +21,7 @@ for file in "$DATADIR"/*; do
 	else
 		# Run RAxML on the file and putting the results in our results folder
 		raxml-ng --msa $file --model GTR+G4
-    		mv ${file}**raxml** ../results/RAxML/
+    		mv ${file}**raxml** ../results/RAxML/gene_trees
 		# Add the file to the completed filenames record
 		echo "$file" >> "$completed_filenames"
 	fi
--- scripts/06-raxml-10mb-concatenation.sh
+++ scripts/06-raxml-10mb-concatenation.sh
@@ -6,7 +6,17 @@ mkdir ../results/
 mkdir ../results/RAxML/
 mkdir ../results/RAxML/10Mb-concatenation
 
+# Store the ones we already did here
+completed_filenames="../results/RAxML/10Mb-concatenation/concat_windows_log_completed_files.txt"
+touch "$completed_filenames"
+
 for file in "$DATADIR"/*; do
-    raxml-ng --msa $file --model GTR+G4 --threads 5
-    mv ${file}**raxml** ../results/RAxML/10Mb-concatenation
+	# Check if RAxML was already run on the file
+	if grep "$file" "$completed_filenames"; then
+		echo "Already did ${file}"
+	else
+		raxml-ng --msa $file --model GTR+G4 --threads 5
+    		mv ${file}**raxml** ../results/RAxML/10Mb-concatenation
+		echo "$file" >> "$completed_filenames"
+	fi
 done
